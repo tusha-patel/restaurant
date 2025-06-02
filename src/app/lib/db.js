e70ed7c123aa45ad8a6ae0803e46d3f58c81c10d
tusha-patel <tusha.b@queueloopsolutions.com>
2025-06-02 11:17:10 +0530

    new

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -2,4 +2,4 @@ const { password } = process.env
 
 
 
-export const connectionStr = "mongodb+srv://tushab:restaurant-next-project08@cluster0.28u8loc.mongodb.net/restaurant_next?retryWrites=true&w=majority&appName=Cluster0";
+export const connectionStr = "mongodb+srv://tushab:" + password + "@cluster0.28u8loc.mongodb.net/restaurant_next?retryWrites=true&w=majority&appName=Cluster0";
